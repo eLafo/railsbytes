1565622479d8f4b4ef6cffa9e467e66acd72ce2e
eLafo <javierlafora@gmail.com>
2022-11-07 00:46:53 +0100

    adds yard

---
+++ yard.rb
@@ -0,0 +1,24 @@
+def do_bundle
+  Bundler.with_original_env { run "bundle install" }
+end
+
+def do_commit
+  git :init
+  git add: "."
+  git commit: " -m 'Adds yard' "
+end
+
+say "\nInstalling yard..."
+inject_into_file 'Gemfile', after: 'group :development do' do
+  <<-RUBY
+
+  # bundler-audit provides patch-level verification for Bundled apps.
+  gem "yard"
+  RUBY
+end
+
+do_bundle
+
+run "bundle binstubs yard"
+
+do_commit
