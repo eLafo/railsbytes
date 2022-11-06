d5ab5cf0229fcf50258ef3f3f5c8ce100ea184a9
eLafo <javierlafora@gmail.com>
2022-11-07 00:46:44 +0100

    adds solargraph

---
+++ solargraph.rb
@@ -0,0 +1,24 @@
+def do_bundle
+  Bundler.with_original_env { run "bundle install" }
+end
+
+def do_commit
+  git :init
+  git add: "."
+  git commit: " -m 'Adds solargraph' "
+end
+
+say "\nInstalling solargraph..."
+inject_into_file 'Gemfile', after: 'group :development do' do
+  <<-RUBY
+
+  # bundler-audit provides patch-level verification for Bundled apps.
+  gem "solargraph"
+  RUBY
+end
+
+do_bundle
+
+run "bundle binstubs solargraph"
+
+do_commit
