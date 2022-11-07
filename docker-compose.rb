02b57df44adcfa5645b8f6b5b456abf4d80b5ef4
eLafo <javierlafora@gmail.com>
2022-11-07 01:04:13 +0100

    removes questions from docker-compose

--- docker-compose.rb
+++ docker-compose.rb
@@ -13,64 +13,58 @@ end
 config["services"] ||=  {}
 config["volumes"] ||= {}
 
-if yes?("Do you want to add a postgres service?")
-  config["volumes"]["postgres"] = {}
-  config["services"]["postgres"] = {
-    "image" => "postgres",
-    "restart" => "always",
-    "environment" => {
-      "POSTGRES_USER" => app_name,
-      "POSTGRES_PASSWORD" => app_name
-    },
-    "volumes" => ["postgres:/var/lib/postgresql"],
-    "ports" => ["5432:5432"]
-  }
+config["volumes"]["postgres"] = {}
+config["services"]["postgres"] = {
+  "image" => "postgres",
+  "restart" => "always",
+  "environment" => {
+    "POSTGRES_USER" => app_name,
+    "POSTGRES_PASSWORD" => app_name
+  },
+  "volumes" => ["postgres:/var/lib/postgresql"],
+  "ports" => ["5432:5432"]
+}
 
-  create_file 'config/database.yml' do <<~EOF
-    default: &default
-      adapter: postgresql
-      encoding: unicode
-      pool: <%= ENV.fetch("RAILS_MAX_THREADS") { 5 } %>
-  
-    development:
-      <<: *default
-      database: #{app_name}_development
-      username: #{app_name}
-      password: #{app_name}
-      host: localhost
-      port: 5432
-  
-    test:
-      <<: *default
-      database: #{app_name}_test
-      username: #{app_name}
-      password: #{app_name}
-      host: localhost
-      port: 5432
-  
-    production:
-      <<: *default
-      url: <%= ENV['DATABASE_URL'] %>
-    EOF
-  end  
-end
+create_file 'config/database.yml' do <<~EOF
+  default: &default
+    adapter: postgresql
+    encoding: unicode
+    pool: <%= ENV.fetch("RAILS_MAX_THREADS") { 5 } %>
 
-if yes?("Do you want a redis service?")
-  config["volumes"]["redis"] = {}
-  config["services"]["redis"] = {
-    "image" => "redis",
-    "restart" => "always",
-    "volumes" => ["redis:/data"],
-    "ports" => ["6379:6379"],
-  }
-end
+  development:
+    <<: *default
+    database: #{app_name}_development
+    username: #{app_name}
+    password: #{app_name}
+    host: localhost
+    port: 5432
 
-if yes?("Do you want a mailcatcher service?")
-  config["services"]["mailcatcher"] = {
-    "image" => "tophfr/mailcatcher",
-    "ports" => ["1080:80", "25:25"]
-  }
-end
+  test:
+    <<: *default
+    database: #{app_name}_test
+    username: #{app_name}
+    password: #{app_name}
+    host: localhost
+    port: 5432
+
+  production:
+    <<: *default
+    url: <%= ENV['DATABASE_URL'] %>
+  EOF
+end  
+
+config["volumes"]["redis"] = {}
+config["services"]["redis"] = {
+  "image" => "redis",
+  "restart" => "always",
+  "volumes" => ["redis:/data"],
+  "ports" => ["6379:6379"],
+}
+
+config["services"]["mailcatcher"] = {
+  "image" => "tophfr/mailcatcher",
+  "ports" => ["1080:80", "25:25"]
+}
 
 create_file "docker-compose.yml", config.to_yaml
 
